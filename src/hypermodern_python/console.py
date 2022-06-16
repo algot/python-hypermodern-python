48d91f30e0389f637c2eb9f5e8e3bac72ff135e6
algot <algot@ua.fm>
2022-06-17 01:40:48 +0300

    simple api request app implemented

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -1,13 +1,23 @@
-import imp
-
+import textwrap
 
 import click
+import requests
 
 from . import __version__
+
+API_URL = "https://uk.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """The hypermodern Python project."""
-    click.echo('Hello, world!')
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
+
+    title = data['title']
+    extract = data['extract']
+
+    click.secho(title, fg='green')
+    click.secho(textwrap.fill(extract))
