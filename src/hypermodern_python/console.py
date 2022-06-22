86e5257a991f653c0ef2f728c052fcc3a880e171
algot <algot@ua.fm>
2022-06-22 17:12:33 +0300

    type annotations and validation added

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -15,13 +15,10 @@ from . import __version__, wikipedia
     show_default=True,
 )
 @click.version_option(version=__version__)
-def main(language):
+def main(language: str) -> None:
     """The hypermodern Python project."""
 
-    data = wikipedia.random_page(language=language)
+    page = wikipedia.random_page(language=language)
 
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.secho(textwrap.fill(extract))
+    click.secho(page.title, fg="green")
+    click.secho(textwrap.fill(page.extract))
