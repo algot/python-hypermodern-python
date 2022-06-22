072332b62942533d39cedf702d0cea79ac025839
algot <algot@ua.fm>
2022-06-22 17:29:05 +0300

    flake8-annotations implemented

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,8 @@
 import tempfile
+from typing import Any
 
 import nox
+from nox.sessions import Session
 
 nox.options.sessions = "lint", "mypy", "pytype", "tests"
 
@@ -10,17 +12,18 @@ package = "hypermodern_python"
 
 
 @nox.session(python=versions)
-def black(session):
+def black(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python=versions)
-def lint(session):
+def lint(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session,
                              "flake8",
+                             "flake8-annotations",
                              "flake8-bandit",
                              "flake8-black",
                              "flake8-bugbear",
@@ -29,7 +32,7 @@ def lint(session):
 
 
 @nox.session(python=versions)
-def tests(session):
+def tests(session: Session) -> None:
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
@@ -39,7 +42,7 @@ def tests(session):
 
 
 @nox.session(python=versions)
-def safety(session):
+def safety(session: Session) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -55,14 +58,14 @@ def safety(session):
 
 
 @nox.session(python=versions)
-def mypy(session):
+def mypy(session: Session) -> None:
     args = session.posargs or ["--ignore-missing-imports", *locations]
     install_with_constraints(session, "mypy")
     session.run("mypy", *args)
 
 
 @nox.session(python=versions)
-def pytype(session):
+def pytype(session: Session) -> None:
     """Run the static type checker."""
     args = session.posargs or ["--disable=import-error", *locations]
     install_with_constraints(session, "pytype")
@@ -70,14 +73,14 @@ def pytype(session):
 
 
 @nox.session(python=versions)
-def typeguard(session):
+def typeguard(session: Session) -> None:
     args = session.posargs or ["-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(session, "pytest", "pytest-mock", "typeguard")
     session.run("pytest", f"--typeguard-packages={package}", *args)
 
 
-def install_with_constraints(session, *args, **kwargs):
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
